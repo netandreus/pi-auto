db5de38d7d5d4e7e5ae5feb7ebbcdb9b33aba97c
Andrey <netandreus@gmail.com>
2026-02-20 15:30:33 +0400

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { z } from "zod";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
